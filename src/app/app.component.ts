9de9d5fed2993fae276aabbfdb4cff6a65b0987e
Unuldur <julien.defiolles@laposte.net>
2024-09-25 11:41:33 +0200

    feat: page-accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccueilComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shiny-stones';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accueil',
+    component: AccueilComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'accueil',
+  },
+];
