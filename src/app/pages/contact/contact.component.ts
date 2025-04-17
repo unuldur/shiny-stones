a6662449e5467316e39ec1cad3ac322a0dea7943
Unuldur <julien.defiolles@laposte.net>
2025-04-17 17:13:26 +0200

    feat: Ajout de la page contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'shiny-stones-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  public nomContact: string = '';
+  public prenomContact: string = '';
+  public objetContact: string = '';
+
+  private get subject(): string {
+    return `Lithotest`;
+  }
+
+  private get body(): string {
+    return `${this.objetContact}%0D%0A%0D%0A${this.prenomContact} ${this.nomContact}`;
+  }
+
+  public get mailTo(): string {
+    return `mailto:shiny.stones@outlook.com?subject=${this.subject}&body=${this.body}`;
+  }
+}
